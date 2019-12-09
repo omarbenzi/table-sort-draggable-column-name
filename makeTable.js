13b568c3faeba282a6b89330da423325279d8c12
omarbenzi <52929955+omarbenzi@users.noreply.github.com>
2019-12-08 22:12:25 -0500

    filter function added

--- makeTable.js
+++ makeTable.js
@@ -1,121 +1,95 @@
 class MakeTable {
-  produits;
+  products;
   tableHeader = [];
+  searchKeyWord;
 
-  constructor(produits) {
-    if (!produits instanceof Array) return
-    this.produits = produits;
-    this.init()
+  constructor(products) {
+    if (!products instanceof Array) return;
+    this.products = products;
+    this.init();
   }
 
   init() {
-    this.getNeededtableHeaderr();
-    this.diaplayTable();
+    this.getNeededtableHeaderr(this.products);
+    //this.filterByKeyWord(this.products, "h");
+    this.diaplayTable(
+      this.tableHeader,
+      this.filterByKeyWord(this.products, "om")
+    );
     this.tableHeader.forEach(e => {
-      this.appliquerLeGlisser(document.getElementById(e))
-    })
+      this.dragAndDropManagement(document.getElementById(e));
+    });
   }
-  getNeededtableHeaderr() {
-    this.produits.forEach((produit) => {
+
+  filterByKeyWord(products, KeyWord) {
+    KeyWord = KeyWord.toString();
+    return products.filter(o =>
+      Object.keys(o).some(
+        k =>
+          (typeof o[k] === "string" &&
+            o[k].toLowerCase().includes(KeyWord.toLowerCase())) ||
+          KeyWord === ""
+      )
+    );
+  }
+
+  getNeededtableHeaderr(products) {
+    products.forEach(produit => {
       Object.keys(produit).forEach(element => {
         if (!this.tableHeader.includes(element)) this.tableHeader.push(element);
       });
     });
   }
 
-  /**
- *  mise en forme HTML du tableau des produits
- *  et insertion dans la section d'id liste
- */
-  diaplayTable() {
+  diaplayTable(tableHeader, products) {
     let listeHTML = "<tr>";
-    this.tableHeader.forEach(e => {
-      listeHTML += `<th id = "${ e }">${ e }</th>`
-    })
+    tableHeader.forEach(e => {
+      listeHTML += `<th id = "${e}">${e}</th>`;
+    });
     listeHTML += "</tr>";
-
-    for (let i = 0; i < this.produits.length; i++) {
-      let p = this.produits[i];
+    for (let i = 0; i < products.length; i++) {
+      let p = products[i];
       listeHTML += "<tr>";
-      this.tableHeader.forEach(e => {
-        listeHTML += "<td>" + p[e] + "</td>"
-      })
+      tableHeader.forEach(e => {
+        listeHTML += "<td>" + p[e] + "</td>";
+      });
       listeHTML += "</tr>";
     }
     document.querySelector(".table").innerHTML = listeHTML;
   }
-  appliquerLeGlisser(element) {
-    let src, srcParent, tgt;
+
+  dragAndDropManagement(element) {
+    let src, tgt;
     element.setAttribute("draggable", true);
-    element.addEventListener("dragstart", function (evt) {
+    element.addEventListener("dragstart", function(evt) {
       evt.dataTransfer.setData("src", evt.target.id);
       evt.target.style.backgroundColor = "gray";
     });
-    element.addEventListener("drag", function (evt) {
+    element.addEventListener("drag", function(evt) {
       evt.target.style.opacity = 0;
     });
-    element.addEventListener("dragend", function (evt) {
+    element.addEventListener("dragend", function(evt) {
       evt.target.style.opacity = 1;
       evt.target.style.backgroundColor = "";
     });
-    element.addEventListener("dragover", function (evt) {
+    element.addEventListener("dragover", function(evt) {
       evt.preventDefault();
     });
-    element.addEventListener("drop", (evt) => {
+    element.addEventListener("drop", evt => {
       evt.preventDefault();
       src = document.getElementById(evt.dataTransfer.getData("src"));
-      srcParent = src.parentNode;
       tgt = evt.currentTarget;
-      this.updateTableHeader(tgt.id, src.id)
+      this.updateTableHeader(tgt.id, src.id);
     });
   }
 
-  updateTableHeader(tgt, drc) {
-    let indexSrc = this.tableHeader.indexOf(drc);
+  updateTableHeader(tgt, src) {
+    let indexSrc = this.tableHeader.indexOf(src);
     let indexTgt = this.tableHeader.indexOf(tgt);
-    [this.tableHeader[indexTgt], this.tableHeader[indexSrc]] = [this.tableHeader[indexSrc], this.tableHeader[indexTgt]];
-    this.init()
+    [this.tableHeader[indexTgt], this.tableHeader[indexSrc]] = [
+      this.tableHeader[indexSrc],
+      this.tableHeader[indexTgt]
+    ];
+    this.init();
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
